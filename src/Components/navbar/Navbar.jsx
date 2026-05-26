641eb4542904d921a8019b910174892916ff7a19
AkhmadRafiHaidarrahman <nakanom858@gmail.com>
2026-05-26 22:22:10 +0700

    navbar

--- src/Components/navbar/Navbar.jsx
+++ src/Components/navbar/Navbar.jsx
@@ -1,22 +1,24 @@
 import "./Navbar.css"
-import Home from "/Tugas RPL/uas_sosmed/src/assets/home.svg"
-import Group from "/Tugas RPL/uas_sosmed/src/assets/group.svg"
-import User from "/Tugas RPL/uas_sosmed/src/assets/user.svg"
+// import Home from "../../assets/home.svg"
+// import Group from "../../assets/group.svg"
+// import User from "../../assets/user.svg"
 // import logo from "/Tugas RPL/uas_sosmed/src/assets/logoUAS.png"
 function Navbar(){
     return(
     <>
-    <nav className="Navbar_omorfos">
-        <div className="navbarHead">
-            <div className="logo">omorfos</div>
-            <ul className="options">
-                <li><button style={{cursor:"pointer"}}><img className="img" src={Home} /></button></li>
-                <li><button style={{cursor:"pointer"}}><img className="img" src={Group} alt="" /></button></li>
-                <li><button style={{cursor:"pointer"}}><img className="img" src={User} alt="" /></button></li>
-                <li><input type="text" className="searchInput
-                " placeholder="search omorfos"/></li>
-            </ul>
-        </div>
+    <nav className="navbar">
+      <div className="navbar-logo">omorfos</div>
+
+      <ul className="navbar-links">
+        <li><button className="nav-btn active"><i className="ti ti-home" /></button></li>
+        <li><button className="nav-btn"><i className="ti ti-users" /></button></li>
+        <li><button className="nav-btn"><i className="ti ti-compass" /></button></li>
+        <li><button className="nav-btn"><i className="ti ti-bell" /></button></li>
+      </ul>
+      <div className="navbar-right">
+        <input type="text" className="searchInput" placeholder="search..." />
+        <div className="avatar">U</div>
+      </div>
     </nav>
     </>
     )
